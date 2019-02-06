121098a0658d7df2a305af11f25d0c4f5c9ab73b
Junaid Atique <junaidatique@gmail.com>
2019-02-06 11:42:33 +0500

    adding react frontend

---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+class BookingsPage extends Component {
+  render() {
+    return (<h1>Bookings</h1>);
+  }
+}
+export default BookingsPage;
---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+class EventsPage extends Component {
+  render() {
+    return (<h1>Events</h1>);
+  }
+}
+export default EventsPage;
